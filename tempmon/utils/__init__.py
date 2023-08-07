383f72f85df6070a4f6c5e80a3b1d4bb12ea0be9
Florian Joerg <florian.joerg@univie.ac.at>
2023-08-07 14:20:24 +0200

    add import

--- tempmon/utils/__init__.py
+++ tempmon/utils/__init__.py
@@ -2,3 +2,5 @@
 # we need this empty __init__.py file
 # If you are curious about the function of the __init__.py file, check out the official python documentation
 # https://docs.python.org/3/reference/import.html#regular-packages
+
+from .preprocess_data import generate_dfs
